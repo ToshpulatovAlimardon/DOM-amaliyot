b57a6e2714e43841f97f40178813593bce51cd4f
Alimardon <alimardontoshpulatov001@gmail.com>
2023-10-10 10:56:13 +0500

    promo__bg changed backround image

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 "use strict";
 
+// 1-exercise
 const promoAdv = document.querySelector(".promo__adv");
 promoAdv.remove();
+
+// 2-exercise
+const promoGenre = document.querySelector(".promo__genre");
+
+promoGenre.textContent = "Komediya";
+
+// 3-exercise
+const promoBg = document.querySelector(".promo__bg");
+
+promoBg.style.cssText =
+    'background: url("../img/1.jpg"); background-position: bottom;';
