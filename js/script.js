5581bc145fa7b735619b355fab50930be021022d
Alimardon <alimardontoshpulatov001@gmail.com>
2023-10-10 11:18:14 +0500

    changed adv remove

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 "use strict";
 
 // 1-exercise
-const promoAdv = document.querySelector(".promo__adv");
-promoAdv.remove();
+const promoAdv = document.querySelectorAll(".promo__adv img");
+promoAdv.forEach((item) => {
+    item.remove();
+});
 
 // 2-exercise
 const promoGenre = document.querySelector(".promo__genre");
@@ -14,3 +16,14 @@ const promoBg = document.querySelector(".promo__bg");
 
 promoBg.style.cssText =
     'background: url("../img/1.jpg"); background-position: bottom;';
+
+// 4-exercise
+// const seriesDB = {
+//     series: [
+//         "OMAR",
+//         "The Final Legacy",
+//         "ERTUGRUL",
+//         "MAGNIFICENT CENTURY",
+//         "GREAT SELJUKS: GUARDIANS...",
+//     ],
+// };
