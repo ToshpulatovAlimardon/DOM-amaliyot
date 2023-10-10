35fa9618e3368a449fa4b5a1f8d0d7a42996c8ba
Alimardon <alimardontoshpulatov001@gmail.com>
2023-10-10 20:24:41 +0500

    solved 4-exercise

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ const promoBg = document.querySelector(".promo__bg");
 promoBg.style.backgroundImage = 'url("img/1.jpg")';
 
 // 4-exercise
+const seriesList = document.querySelector(".promo__interactive-list");
+const seriesDb = {
+    series: [
+        "Omar",
+        "The Final Legacy",
+        "Ertugrul",
+        "Magnificent Century",
+        "The Great Seljuks: Guardians...",
+    ],
+};
+
+seriesList.innerHTML = "";
+seriesDb.series.forEach((item, index) => {
+    seriesList.innerHTML += `
+    <li class="promo__interactive-item"> ${index + 1} ${item}
+        <div class="delete"></div>
+    </li>
+    `;
+});
